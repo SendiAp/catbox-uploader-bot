5eda1e5dc05ee876e235b21815224c196fa50ffa
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-10-19 22:52:16 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 12345678))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("API_ID", "20211998"))
+    API_HASH = os.environ.get("API_HASH", "beeeebe74c0c467c47c6ac4a1c9d75b5")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7701888542:AAGyc0EMEUji1s4cYvQM5Ma2WC3XBxPhM3A")
